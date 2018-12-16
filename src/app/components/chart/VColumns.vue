e1e0ceb50386a7a9cae3389d75eb9ba12c9807b1
Luis Palomino Trevilla <luispalominot3@gmail.com>
2018-12-16 01:00:49 -0600

    Finished basic Costs graph

--- src/app/components/chart/VColumns.vue
+++ src/app/components/chart/VColumns.vue
@@ -23,6 +23,21 @@
                 :options="ColumnOptions"
                 ref="columnCharts">
             </vue-highcharts >
+            <b-row
+                class="text-center"
+                v-if="!dangerAlert">
+                <b-col class="column-legends">
+                    <ul>
+                    <li
+                        v-for="(rate, index) in rate_types"
+                        :key=index
+                        class="legend">
+                        <div class="square" :style="{backgroundColor: colors[index]}"></div>
+                        <p class="legend-text">{{rate}}</p>
+                    </li>
+                    </ul>
+                </b-col>
+            </b-row>
             <b-alert
                 v-else
                 show
@@ -39,6 +54,7 @@ import VueHighcharts from 'vue2-highcharts';
 import meters from '@/services/meters';
 
 const weekLabels = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
+const colors = {'base': '#eddc49', 'middle': '#1dd6c0', 'peak': '#db3c1c'};
 
 var dataColumn = {
     chart: {
@@ -48,7 +64,7 @@ var dataColumn = {
         text: null
     },
     xAxis: {
-        categories: ['Jan', 'Feb']
+        categories: []
     },
     yAxis: {
         title: {
@@ -57,19 +73,40 @@ var dataColumn = {
     },
     tooltip: {
         crosshairs: true,
-        shared: true
+        shared: true,
+        pointFormat: '<span style="color:{point.color}">\u25CF</span> Costo: <b>${point.y}</b><br/>',
+        valueSuffix: ' MXN'
     },
     credits: {
         enabled: false
     },
     plotOptions: {
+        column: {
+            groupPadding: 0
+        },
         series: {
             colorByPoint: true
         }
     },
     series: [{
-        data: [{name: "hola", y: 20, color: "#ff4563"}, {name: "mundo", y: 60, color: "#ff4ab3"}]
+        data: []
     }]
+};
+
+function parseDate(rawDate) {
+    let day, month, year;
+    day = rawDate.substring(0, 2);
+    month = rawDate.substring(2, 4);
+    year = rawDate.substring(4, 8);
+    return `${day}/${month}/${year}`;
+}
+
+function parseDateTime(rawDate) {
+    let hour, minute, second;
+    hour = rawDate.substring(8, 10);
+    minute = rawDate.substring(10, 12);
+    second = rawDate.substring(12, 14);
+    return `${hour}:${minute}:${second}`;
 }
 
 export default {
@@ -102,13 +139,22 @@ export default {
                 ]
             },
             currentPeriod: 0,
-            dangerAlert: false
+            dangerAlert: false,
+            rate_types: [
+                'Base',
+                'Intermedia',
+                'Punta'
+            ],
+            colors: [
+                '#eddc49',
+                '#1dd6c0',
+                '#db3c1c'
+            ]
         }
     },
 
     watch: {
         meterId() {
-            console.log("WUJU")
             this.changePeriod(0);
         }
     },
@@ -150,7 +196,18 @@ export default {
             let meter_device = (meter[1] === "EDS")? "":meter[1];
             meters.getConsumptionCostsByFilter(meter_id, meter_device, filter)
                 .then(res => {
-                    console.log(res);
+                    if (res) {
+                        let data = [];
+                        let xAxis = res.map(item => {
+                            const time = parseDateTime(item.date);
+                            data.push({name: `${item.rate} - ${time}`, y: parseFloat(item.cost), color: colors[item.rate]})
+                            return time;
+                        });
+                        chart.update({
+                            xAxis: { categories: xAxis }
+                        });
+                        this.updateSeries(data);
+                    }
                 })
                 .catch(err => {
                     console.log(err);
