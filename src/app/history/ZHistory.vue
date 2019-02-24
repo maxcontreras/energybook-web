10e448b7deef42f893def94bf8e7c1a744f2c5c2
Luis Palomino Trevilla <luispalominot3@gmail.com>
2019-02-24 14:33:02 -0600

    Show previous monthlyReadings

--- src/app/history/ZHistory.vue
+++ src/app/history/ZHistory.vue
@@ -44,15 +44,29 @@
                             <b-col
                                 cols="12"
                                 class="text-center">
-                                <b-button class="btn-confirmation">Confirmar</b-button>
+                                <b-button
+                                    :disabled="isLoading"
+                                    class="btn-confirmation"
+                                    @click="getMonthlyHistory">
+                                    Confirmar
+                                </b-button>
                             </b-col>
                         </b-row>
                     </b-col>
                     <b-col cols="12">
-                        <!-- <div class="loading-spinner text-center">
+                        <div
+                            v-if="isLoading"
+                            class="loading-spinner text-center">
                             <img src="/assets/images/loading.svg" alt="loading">
-                        </div> -->
-                        <b-row class="monthly-readings">
+                        </div>
+                        <div
+                            v-if="noData"
+                            class="no-data-alert">
+                            <b-alert class="tmp-alert" variant="warning" show>No hay datos que mostrar para el mes seleccionado</b-alert>
+                        </div>
+                        <b-row
+                            v-if="!isLoading && hasData && !noData"
+                            class="monthly-readings">
                             <b-col>
                                 <div class="analysis-item--cat">
                                     <div class="icon-container">
@@ -62,12 +76,12 @@
                                         <p>Consumo</p>
                                         <b-row>
                                             <b-col>
-                                                <h5></h5>
+                                                <h5>{{data.consumption}}</h5>
                                                 <span>kWh</span>
                                             </b-col>
                                             <b-col>
                                                 <span>$</span>
-                                                <h5></h5>
+                                                <h5>{{consumptionCost}}</h5>
                                             </b-col>
                                         </b-row>
                                     </div>
@@ -80,12 +94,12 @@
                                         <p>Distribución</p>
                                         <b-row>
                                             <b-col>
-                                                <h5></h5>
+                                                <h5>{{data.distribution}}</h5>
                                                 <span>kW</span>
                                             </b-col>
                                             <b-col>
                                                 <span>$</span>
-                                                <h5></h5>
+                                                <h5>{{distributionCost}}</h5>
                                             </b-col>
                                         </b-row>
                                     </div>
@@ -98,12 +112,12 @@
                                         <p>Capacidad</p>
                                         <b-row>
                                             <b-col>
-                                                <h5></h5>
+                                                <h5>{{data.capacity}}</h5>
                                                 <span>kW</span>
                                             </b-col>
                                             <b-col>
                                                 <span>$</span>
-                                                <h5></h5>
+                                                <h5>{{capacityCost}}</h5>
                                             </b-col>
                                         </b-row>
                                     </div>
@@ -116,7 +130,7 @@
                                         <p>F.P</p>
                                         <b-row>
                                             <b-col cols="6">
-                                                <h5></h5>
+                                                <h5>{{data.fp}}</h5>
                                                 <span>%</span>
                                             </b-col>
                                         </b-row>
@@ -132,12 +146,20 @@
 </template>
 
 <script>
+import Services from '@/services/services';
+import meters from '@/services/meters'
+import designatedMeters from '@/services/designatedMeters'
+import Constants from '@/constants.json';
 
 export default {
     data() {
         return {
             date: moment().startOf('month').subtract(1, 'month').format(),
-            service: 0
+            service: 0,
+            isLoading: false,
+            data: {},
+            noData: false,
+            consumptionCost: 0
         };
     },
     computed: {
@@ -156,11 +178,21 @@ export default {
         },
         services() {
             return this.$store.state.services.map((service, index) => ({value: index, text: service}));
-        }
-    },
-    watch: {
-        data() {
-
+        },
+        companyId() {
+            return this.$store.state.company_id;
+        },
+        hasData() {
+            return this.data.consumption && this.data.distribution && this.data.capacity && this.data.fp;
+        },
+        cfePrices() {
+            return this.$store.getters['meter/getCfePrices'];
+        },
+        distributionCost() {
+            return (this.cfePrices.distribution * parseFloat(this.data.distribution)).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        },
+        capacityCost() {
+            return (this.cfePrices.capacity * parseFloat(this.data.capacity)).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         }
     },
     methods: {
@@ -169,6 +201,46 @@ export default {
             if (moment().startOf('month').isAfter(new_date)) {
                 this.date = new_date;
             }
+        },
+        getMonthlyHistory() {
+            this.noData = false;
+            this.isLoading = true;
+            const serviceSelected = this.services[this.service].text;
+            Services.monthlyHistory(serviceSelected, this.companyId, this.date)
+                .then(({data}) => {
+                    if (data) {
+                        let custom_dates = {
+                            from: moment(this.date).startOf('month').format(),
+                            until: moment(this.date).endOf('month').format()
+                        }
+                        designatedMeters.find({
+                            filter: {
+                                where: {
+                                    company_id: this.companyId
+                                }
+                            }
+                        }).then(mets => {
+                            let edsId = mets[0].meter_id;
+                            meters.getConsumptionCostsByFilter(edsId, '', serviceSelected, Constants.Meters.filters.custom, 86400, custom_dates)
+                            .then(res => {
+                                let cost = (res.reduce((prev, curr) => {
+                                    return prev + parseFloat(curr.cost);
+                                }, 0)).toFixed(2);
+                                this.consumptionCost = cost.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                            });
+                        });
+                        this.data = data;
+                    } else {
+                        this.noData = true;
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                    this.noData = true;
+                })
+                .finally(() => {
+                    this.isLoading = false;
+                });
         }
     }
 }
@@ -178,6 +250,12 @@ export default {
 $darkgray: #485658;
 
 #readings-history {
+
+    .loading-spinner,
+    .no-data-alert {
+        margin-top: 6rem;
+        margin-bottom: 6rem;
+    }
 
     .monthly-readings {
         margin-top: 4rem;
