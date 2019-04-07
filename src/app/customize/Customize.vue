15baea5a83fe38769553f6cdd40afcc266e61827
Luis Palomino Trevilla <luispalominot3@gmail.com>
2019-04-06 21:07:53 -0600

    Created form to upload pdfs

--- src/app/customize/Customize.vue
+++ src/app/customize/Customize.vue
@@ -1,19 +1,71 @@
 <template>
     <b-row id="customzie" class="main">
-        <b-col>
-            <p>Hellow</p>
+        <b-col lg="7">
+            <b-card no-body>
+                <b-row class="card-header">
+                    <b-col>
+                        <h5>Pruebas de software</h5>
+                    </b-col>
+                </b-row>
+                <b-row class="card-body">
+                    <b-col>
+                    </b-col>
+                </b-row>
+            </b-card>
+        </b-col>
+        <b-col lg="5">
+            <b-card
+                class="information-files"
+                no-body>
+                <b-row class="card-header">
+                    <b-col>
+                        <h5>Archivos de Información</h5>
+                    </b-col>
+                </b-row>
+                <b-row class="card-body">
+                    <b-col cols="12">
+                        <h3>Agregar archivo</h3>
+                        <pdf-form />
+                    </b-col>
+                    <b-col>
+                        <h3>Archivos en línea</h3>
+                    </b-col>
+                </b-row>
+            </b-card>
         </b-col>
     </b-row>
 </template>
 
 <script>
+import PdfForm from './PDForm.vue';
+
 export default {
-    
+    components: {
+        PdfForm
+    }
 }
 </script>
 
-<style lang="scss">
-    .tmp-alert {
-        font-size: 15px;
+<style lang="scss" scoped>
+    .card-header {
+        h5 {
+            font-size: 1.1rem;
+            letter-spacing: 1px;
+            font-weight: bold;
+        }
+
+        border-top-left-radius: 0.4rem;
+        border-top-right-radius: 0.4rem;
+        padding: 1.5rem 2rem 1rem;
+        background-color: #ffffff;
+        border-bottom: 1px solid #999999;
+        margin: 0;
+    }
+
+    .information-files {
+        h3 {
+            font-size: 1rem;
+            letter-spacing: 1px;
+        }
     }
 </style>
---
+++ src/app/customize/PDForm.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="create-pdf-container">
+        <b-form-group
+            label="Título del PDF"
+            label-for="pdf-title">
+            <b-form-input
+                id="pdf-title"
+                v-model="pdf.title"
+                type="text"/>
+        </b-form-group>
+        <b-form-group
+            label="Link al archivo"
+            label-for="pdf-link">
+            <b-form-input
+                id="pdf-link"
+                v-model="pdf.link"
+                type="text"/>
+        </b-form-group>
+        <b-form-group
+            label="Descripción"
+            label-for="pdf-description">
+            <b-form-textarea
+                id="pdf-description"
+                size="lg"
+                v-model="pdf.description"
+                type="text"/>
+        </b-form-group>
+        <b-form-group
+            label="Número de artículo"
+            label-for="pdf-number">
+            <b-form-input
+                id="pdf-number" 
+                v-model="pdf.number"
+                type="number"/>
+        </b-form-group>
+        <b-btn
+            @click="createPDF">
+            Subir archivo
+        </b-btn>
+    </div>
+</template>
+
+<script>
+import notify from '@/mixins/notify';
+
+export default {
+
+    mixins: [notify('notification')],
+    
+    data() {
+        return {
+            pdf: {
+                title: '',
+                link: '',
+                description: '',
+                number: ''
+            }
+        }
+    },
+
+    methods: {
+
+        validateFields() {
+            const httpRegexp = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/|www\.){1}[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm;
+            const inputsFilled = this.pdf.title.replace(/\s/g,"") !== "" &&
+                                this.pdf.description.replace(/\s/g,"") !== "" &&
+                                this.pdf.link.replace(/\s/g,"") !== "" &&
+                                this.pdf.number.replace(/\s/g,"") !== "";
+            const linkValid = httpRegexp.test(this.pdf.link);
+            return inputsFilled && linkValid;
+        },
+
+        createPDF() {
+            if (this.validateFields()) {
+
+            } else {
+                this.notify('No se pudo subir el PDF', 'Revisa los datos', 'warn');
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .create-pdf-container {
+        margin: 3rem 0;
+        padding: 0 10%;
+    }
+    #pdf-description {
+        font-size: 1.1rem;
+    }
+</style>
