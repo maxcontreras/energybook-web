4700adc51359882f97290cead608c6dc3d9303c1
Luis Palomino Trevilla <luispalominot3@gmail.com>
2019-04-10 18:11:22 -0500

    Admin view to select trial company is finished

--- src/app/customize/FreeTrial.vue
+++ src/app/customize/FreeTrial.vue
@@ -2,11 +2,13 @@
     <b-row>
         <b-col lg="6">
             <h3>Compañía designada para pruebas de software</h3>
-            <ul v-if="freeTrialCompany">
-                <li :key="company.id" v-for="company in freeTrialCompany">
-                    {{ company.company_name }}
-                </li>
-            </ul>
+            <h5 v-if="freeTrialCompany">
+                <i class="fas fa-building"></i>
+                {{ freeTrialCompany.company_name }}
+                <i
+                    class="fas fa-times-circle"
+                    @click="removeCompany"></i>
+            </h5>
             <b-alert
                 v-else
                 show
@@ -17,7 +19,13 @@
         <b-col lg="6">
             <h3>Designar compañía</h3>
             <b-form-select v-model="selectedCompany" :options="companies"/>
-            <b-btn>Designar</b-btn>
+            <div class="text-center">
+                <b-btn
+                    class="designate-btn"
+                    @click="designateCompany">
+                    Designar
+                </b-btn>
+            </div>
         </b-col>
     </b-row>
 </template>
@@ -63,14 +71,74 @@ export default {
                 .catch(err => {
                     console.log(err);
                 });
+        },
+
+        designateCompany() {
+            if (this.freeTrialCompany) {
+                company.patch({ id: this.freeTrialCompany.id }, { for_free_trial: false })
+                    .then(updated => {
+                        this.raw_companies.push(updated);
+                        return company.patch({ id: this.selectedCompany }, { for_free_trial: true })
+                    })
+                    .then((freetrialCompany) => {
+                        this.freeTrialCompany = freetrialCompany;
+                        this.raw_companies = this.raw_companies.filter(company => company.id !== this.selectedCompany);
+                        this.selectedCompany = this.raw_companies[0] && this.raw_companies[0].id;
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+            } else {
+                company.patch({ id: this.selectedCompany }, { for_free_trial: true })
+                    .then((freetrialCompany) => {
+                        this.freeTrialCompany = freetrialCompany;
+                        this.raw_companies = this.raw_companies.filter(company => company.id !== this.selectedCompany);
+                        this.selectedCompany = this.raw_companies[0] && this.raw_companies[0].id;
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+            }
+            
+        },
+
+        removeCompany() {
+            company.patch({ id: (this.freeTrialCompany) ? this.freeTrialCompany.id : '' }, { for_free_trial: false })
+                .then(updated => {
+                    this.freeTrialCompany = null;
+                    this.raw_companies.push(updated);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
         }
     }
 }
 </script>
 
 <style lang="scss" scoped>
+
 h3 {
     font-size: 1.1rem;
     margin-bottom: 2rem;
+}
+
+.designate-btn {
+    margin-top: 2rem;
+}
+
+.custom-select {
+    padding-bottom: 3px;
+}
+
+.fa-building {
+    margin-right: 1rem;
+}
+
+.fa-times-circle {
+    position: absolute;
+    right: 0;
+    color: rgb(216, 25, 25);
+    cursor: pointer;
 }
 </style>
