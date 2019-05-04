0e6b6973fa2388d7d824c5934ec934c96e62eabc
Luis Palomino Trevilla <luispalominot3@gmail.com>
2019-05-04 17:53:31 -0500

    Created basic generation page layout

--- src/app/generation/Generation.vue
+++ src/app/generation/Generation.vue
@@ -1,19 +1,135 @@
 <template>
     <b-row id="energy-generation">
         <b-col>
-            <b-alert class="tmp-alert" variant="info" show>Estamos trabajando para ti, espera pronto esta nueva sección</b-alert>
+            <b-row>
+                <b-col xl="4" lg="6">
+                    <reading-card>
+                        <template v-slot:right-header>
+                            <h5>
+                                {{ currentDay }}
+                            </h5>
+                        </template>
+                        <template v-slot:body>
+                            <b-row class="properties">
+                                <b-col cols="12">
+                                    <v-property
+                                        property-name="Generacion"
+                                        property-value="100,000"
+                                        property-unit="kWh"/>
+                                </b-col>
+                                <b-col cols="12">
+                                    <v-property
+                                        property-name="Auto consumo"
+                                        property-value="200"
+                                        property-unit="kWh"/>
+                                </b-col>
+                                <b-col cols="12">
+                                    <v-property
+                                        property-name="Inyección a la red"
+                                        property-value="400"
+                                        property-unit="kWh"/>
+                                </b-col>
+                            </b-row>
+                        </template>
+                    </reading-card>
+                </b-col>
+                <b-col xl="4" lg="6">
+                    <reading-card>
+                        <template v-slot:right-header>
+                            <h5>
+                                {{ currentDay }}
+                            </h5>
+                        </template>
+                        <template v-slot:body>
+                            <b-row class="properties">
+                                <b-col cols="12">
+                                    <v-property
+                                        property-name="Generacion"
+                                        property-value="$100.00"/>
+                                </b-col>
+                                <b-col cols="12">
+                                    <v-property
+                                        property-name="Auto consumo"
+                                        property-value="$200.00"/>
+                                </b-col>
+                                <b-col cols="12">
+                                    <v-property
+                                        property-name="Inyección a la red"
+                                        property-value="$400.00"/>
+                                </b-col>
+                            </b-row>
+                        </template>
+                    </reading-card>
+                </b-col>
+                <b-col xl="4" lg="6">
+                    <reading-card>
+                        <template v-slot:right-header>
+                            <h5>
+                                {{ currentDay }}
+                            </h5>
+                        </template>
+                        <template v-slot:body>
+                            <b-row class="properties">
+                                <b-col cols="12">
+                                    <v-property
+                                        property-name="Emisiones de CO2"
+                                        property-value="100,000"
+                                        property-unit="kWh"/>
+                                </b-col>
+                                <b-col cols="12">
+                                    <v-property
+                                        property-name="Emisiones de CH4"
+                                        property-value="200"
+                                        property-unit="kWh"/>
+                                </b-col>
+                                <b-col cols="12">
+                                    <v-property
+                                        property-name="Emisiones N2O"
+                                        property-value="400"
+                                        property-unit="kWh"/>
+                                </b-col>
+                            </b-row>
+                        </template>
+                    </reading-card>
+                </b-col>
+            </b-row>
         </b-col>
     </b-row>
 </template>
 
 <script>
+import ReadingCard from '@/app/components/ReadingCard';
+import VProperty from './VProperty';
+
 export default {
-    
+    components: {
+        ReadingCard,
+        VProperty
+    },
+
+    data() {
+        return {
+
+        };
+    },
+
+    computed: {
+        currentDay() {
+            moment().locale();
+            return moment().format('dddd D [de] MMMM');
+        }
+    }
 }
 </script>
 
-<style lang="scss">
-    .tmp-alert {
-        font-size: 15px;
+<style lang="scss" scoped>
+.properties {
+    > * {
+        margin-bottom: .5rem;
+
+        &:first-child {
+            margin-top: .5rem;
+        }
     }
+}
 </style>
---
+++ src/app/generation/VProperty.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="record">
+        <div class="property">
+            <i class="fas fa-leaf"></i>
+            <h5>{{propertyName}}</h5>
+        </div>
+        <div class="value">
+            <h5>{{propertyValue}} {{propertyUnit}}</h5>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        propertyName: {
+            type: String,
+            required: true
+        },
+        propertyValue: {
+            type: String,
+            default: 0
+        },
+        propertyUnit: {
+            type: String,
+            required: false
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.record {
+    padding: 0 3rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    h5 {
+        font-size: 1.2rem;
+    }
+
+    .property {
+        * {
+            display: inline-block;
+        }
+
+        i {
+            font-size: 1.3rem;
+            color: #1d4e10;
+            margin-right: 1rem;
+        }
+    }
+}
+</style>
