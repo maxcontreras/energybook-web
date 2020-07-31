d0d6628bb58355facf92f425a9b61008264b89ce
juanLlantada <juanp@llantada.com>
2020-07-31 15:35:46 -0600

    Google tab Manager en Vuejs

--- src/index.js
+++ src/index.js
@@ -1,29 +1,38 @@
 /* eslint-disable */
-import Vue from 'vue'
-import router from './router.js'
-import store from './store/store.js'
-import BootstrapVue from 'bootstrap-vue'
-import * as VueGoogleMaps from 'vue2-google-maps'
-import Notifications from 'vue-notification'
-import 'pc-bootstrap4-datetimepicker/build/css/bootstrap-datetimepicker.css'; // eslint-disable-line
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vue from "vue";
+import router from "./router.js";
+import VueGtm from "vue-gtm";
+import store from "./store/store.js";
+import BootstrapVue from "bootstrap-vue";
+import * as VueGoogleMaps from "vue2-google-maps";
+import Notifications from "vue-notification";
+import "pc-bootstrap4-datetimepicker/build/css/bootstrap-datetimepicker.css"; // eslint-disable-line
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-import './index.scss'
+import "./index.scss";
 
-Vue.use(BootstrapVue)
-Vue.use(Notifications)
+Vue.use(VueGtm, {
+  id: "GTM-5ZRBN2V", // Your GTM single container ID or array of container ids ['GTM-xxxxxxx', 'GTM-yyyyyyy']
+  defer: false, // defaults to false. Script can be set to `defer` to increase page-load-time at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible)
+  enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
+  debug: true, // Whether or not display console logs debugs (optional)
+  loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional)
+});
+
+Vue.use(BootstrapVue);
+Vue.use(Notifications);
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyBBkQHeDuyANETUPLQ9tvgKaQzSHLiaj2k', 
-    libraries: 'places'
-  }
-})
+    key: "AIzaSyBBkQHeDuyANETUPLQ9tvgKaQzSHLiaj2k",
+    libraries: "places",
+  },
+});
 
-store.dispatch('user/syncRouter', router)
+store.dispatch("user/syncRouter", router);
 
 export default new Vue({
-  el: '#root',
+  el: "#root",
   router,
   store,
-  render: h => h('router-view')
-})
+  render: (h) => h("router-view"),
+});
